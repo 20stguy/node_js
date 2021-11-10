4f02cd25eafb0d418a740ccda45414bdadcd59ad
20stguy <20stguy+git@gmail.com>
2021-11-10 14:01:47 +0900

    Fixed conflicts2 Updating 'index.js', 'serve.js', 'path.route.js', 'request.handler.js', explorers - 'test.index.js', 'test.server.js', 'test.request.handler.js'

--- path.routes.js
+++ path.routes.js
@@ -1,11 +1,11 @@
 function route(handle, pathname){
-    console.log("This is from test.routes functions route");
-    console.log("About to route a request for " + pathname);
+    console.log("This is from test.routes functions route About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
         handle[pathname]();
     } else {
         console.log("No request handler found for " + pathname);
-    }
+    return "404 Not found from path.routes.js.route";
+    };
 };
 
 module.exports = {
